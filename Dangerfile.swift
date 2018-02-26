b169e4125f63aa2507dac96336f9424864ceac4e
Sven Tiigi <sven.tiigi@gmail.com>
2018-02-26 15:34:42 +0100

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,5 +2,25 @@ import Foundation
 import Danger
 import DangerSwiftLint
 
-// Run SwiftLint for FlyoverKit with configuration
+// Initialize Danger
+let danger = Danger()
+
+// MARK: - Pull Request
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+if danger.github.pullRequest.title.contains("WIP") {
+    warn("PR is classed as Work in Progress")
+}
+
+// MARK: - Issue
+
+if danger.github.issue.body.isEmpty {
+    warn("Please provide a issue description")
+}
+
+// MARK: - SwiftLint
+
 SwiftLint.lint(directory: "FlyoverKit", configFile: ".swiftlint.yml")
