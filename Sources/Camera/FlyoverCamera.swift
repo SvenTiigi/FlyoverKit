4b5ca14fc3f638ee867e0101acaf604936412db6
Sven Tiigi <sven.tiigi@gmail.com>
2018-09-13 12:18:30 +0200

    Added weak self

--- Sources/Camera/FlyoverCamera.swift
+++ Sources/Camera/FlyoverCamera.swift
@@ -73,14 +73,14 @@ open class FlyoverCamera {
         // Add application will resign active observer
         NotificationCenter.default.addObserver(
             self,
-            selector: #selector(applicationWillResignActive),
+            selector: #selector(self.applicationWillResignActive),
             name: .UIApplicationWillResignActive,
             object: nil
         )
         // Add application did become active observer
         NotificationCenter.default.addObserver(
             self,
-            selector: #selector(applicationDidBecomeActive),
+            selector: #selector(self.applicationDidBecomeActive),
             name: .UIApplicationDidBecomeActive,
             object: nil
         )
@@ -125,14 +125,20 @@ open class FlyoverCamera {
             let startAnimator = UIViewPropertyAnimator(
                 duration: duration,
                 curve: curve,
-                animations: {
+                animations: { [weak self] in
+                    // Verify self is available
+                    guard let strongSelf = self else {
+                        // Self isn't available return out of function
+                        return
+                    }
                     // Set MapView Camera
-                    self.mapView?.camera = self.mapCamera
-            })
+                    strongSelf.mapView?.camera = strongSelf.mapCamera
+                }
+            )
             // Add completion
-            startAnimator.setCompletion {
+            startAnimator.setCompletion { [weak self] in
                 // Start rotation
-                self.performFlyover(flyover)
+                self?.performFlyover(flyover)
             }
             // Start animation
             startAnimator.startAnimation()
@@ -165,15 +171,20 @@ open class FlyoverCamera {
         self.animator = UIViewPropertyAnimator(
             duration: 0,
             curve: self.curve,
-            animations: {
+            animations: { [weak self] in
+                // Verify self is available
+                guard let strongSelf = self else {
+                    // Self isn't available return out of function
+                    return
+                }
                 // Substract the headingStep from current heading to retrieve start value
-                heading -= self.configuration.headingStep
+                heading -= strongSelf.configuration.headingStep
                 // Initialize the percentage of the compeleted heading step
-                let percentageCompletedHeadingStep = Double(fractionComplete) * self.configuration.headingStep
+                let percentageCompletedHeadingStep = Double(fractionComplete) * strongSelf.configuration.headingStep
                 // Set MapCamera Heading
-                self.mapCamera.heading = fmod(heading + percentageCompletedHeadingStep, 360)
+                strongSelf.mapCamera.heading = fmod(heading + percentageCompletedHeadingStep, 360)
                 // Set MapView Camera
-                self.mapView?.camera = self.mapCamera
+                strongSelf.mapView?.camera = strongSelf.mapCamera
         })
         // Start animation
         self.animator?.startAnimation()
@@ -198,16 +209,21 @@ open class FlyoverCamera {
         self.animator = UIViewPropertyAnimator(
             duration: self.configuration.duration,
             curve: self.curve,
-            animations: {
+            animations: { [weak self] in
+                // Verify self is available
+                guard let strongSelf = self else {
+                    // Self isn't available return out of function
+                    return
+                }
                 // Update MapViewCamera
-                self.mapView?.camera = self.mapCamera
+                strongSelf.mapView?.camera = strongSelf.mapCamera
         })
         // Add completion
-        self.animator?.setCompletion {
+        self.animator?.setCompletion { [weak self] in
             // Check if flyovers are nearly equal
-            if self.flyover ~~ flyover {
+            if self?.flyover ~~ flyover {
                 // Invoke recursion
-                self.performFlyover(flyover)
+                self?.performFlyover(flyover)
             }
         }
         // Start Animation
