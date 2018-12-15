3a02f75563f7a6bef73c7773a21c6813f0292e2c
Sven Tiigi <sven.tiigi@gmail.com>
2018-12-15 16:17:55 +0100

    Updated Code Layout

--- Sources/Flyover.swift
+++ Sources/Flyover.swift
@@ -19,57 +19,83 @@ public protocol Flyover {
 // MARK: - CoreLocation Framework Flyover Extensions
 
 extension CLLocationCoordinate2D: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self
     }
+    
 }
+
 extension CLCircularRegion: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.center
     }
+    
 }
+
 extension CLLocation: Flyover { }
 
 // MARK: - MapKit Framework Flyover Extensions
 
 extension MKMapItem: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.placemark.coordinate
     }
+    
 }
+
 extension MKMapView: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.centerCoordinate
     }
+    
 }
+
 extension MKMapPoint: Flyover {}
+
 extension MKCoordinateRegion: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.center
     }
+    
 }
+
 extension MKMapRect: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.origin.coordinate
     }
+    
 }
+
 extension MKCoordinateSpan: Flyover {
+    
     /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
-        return CLLocationCoordinate2D(latitude: self.latitudeDelta, longitude: self.longitudeDelta)
+        return .init(latitude: self.latitudeDelta, longitude: self.longitudeDelta)
     }
+    
 }
+
 extension MKMapCamera: Flyover {
+    
     /// /// The flyover coordinate
     public var coordinate: CLLocationCoordinate2D {
         return self.centerCoordinate
     }
+    
 }
+
 extension MKShape: Flyover { }
+
 extension MKPlacemark: Flyover { }
